1edfc36e4f632b1bca87cd5e970ed4ed020cdde4
Thurunany <thutymsoares@gmail.com>
2022-06-08 23:06:59 -0300

    Basic Hello World Program

---
+++ helloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Hello World!\n");   //Basic print the wrote phrase
+    return 0;                   //To end the program 
+}
+/* 
+All done! To run the program just write in the terminal:
+gcc fileName.c  <-compile
+./a.out         <- show results!
+*/
