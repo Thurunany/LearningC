7679c4f17c0366379d635a7daa6f31630f8d96a2
Thurunany <thutymsoares@gmail.com>
2022-06-08 23:26:18 -0300

    Learning Variables

---
+++ variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char name[] = "Maria";
+    int age = 25;
+
+    printf("There once was a girl named %s. \n", name);    
+    printf("She was %d years old. \n", age);
+    printf("She really liked the name %s. \n", name);
+    return 0;
+}
